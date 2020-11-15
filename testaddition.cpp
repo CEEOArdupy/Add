e5dda10396f97ebf55587fbd3194d58a08abdc04
Milan <milandahal213@gmail.com>
2020-11-14 23:53:05 -0500

    Update testaddition.cpp

--- testaddition.cpp
+++ testaddition.cpp
@@ -1,10 +1,10 @@
 #include "testaddition.h"
 
 add::add(void) {
-	uint32_t dummy=0;
+	int32_t dummy=0;
 }
 
-uint32_t add::sum(uint32_t a, uint32_t b) {
-    uint32_t c = a+b;
+int32_t add::sum(int32_t a, int32_t b) {
+    int32_t c = a+b;
     return c;
 }
